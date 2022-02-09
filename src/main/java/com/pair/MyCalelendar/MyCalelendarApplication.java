583e4bce83809d894ea0507d29fa6848ed932be8
ttasjwi <ttasjwi920@gmail.com>
2022-02-09 14:47:42 +0900

    init project

---
+++ src/main/java/com/pair/MyCalelendar/MyCalelendarApplication.java
@@ -0,0 +1,13 @@
+package com.pair.MyCalelendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCalelendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCalelendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pair/MyCalelendar/MyCalelendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pair.MyCalelendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCalelendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
