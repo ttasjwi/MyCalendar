ebee1689353acf7c4872f980097a7dc18df7e6e7
who-hoo <who.ho3ov@gmail.com>
2022-02-09 15:25:44 +0900

    MyCalendar에 년월일 날짜를 문자열로 입력하면 LocalDate 인스턴스를 반환하는 of 메서드 작성

--- src/main/java/com/pair/MyCalelendar/MyCalendar.java
+++ src/main/java/com/pair/MyCalelendar/MyCalendar.java
@@ -1,12 +1,19 @@
 package com.pair.MyCalelendar;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class MyCalendar {
 
     public static LocalDateTime now() {
         LocalDateTime now = LocalDateTime.now();
-        return LocalDateTime.of(now.getYear(), now.getMonthValue(), now.getDayOfMonth(), now.getHour(), now.getMinute(),now.getSecond());
+        return LocalDateTime.of(now.getYear(), now.getMonthValue(), now.getDayOfMonth(),
+            now.getHour(), now.getMinute(), now.getSecond());
     }
 
+    public static LocalDate of(String str) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return LocalDate.parse(str, formatter);
+    }
 }
--- src/test/java/com/pair/MyCalelendar/MyCalendarTest.java
+++ src/test/java/com/pair/MyCalelendar/MyCalendarTest.java
@@ -1,6 +1,8 @@
 package com.pair.MyCalelendar;
 
+import java.time.LocalDate;
 import net.bytebuddy.asm.Advice;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +18,25 @@ class MyCalendarTest {
         //given
         LocalDateTime now = MyCalendar.now();
         System.out.println(now);
+    }
+
+    @Test
+    @DisplayName("년월일 날짜만 문자열로 입력하면 Date 인스턴스를 리턴한다.")
+    void of() {
+        //given
+        String str = "2022-02-09";
+        LocalDate date = MyCalendar.of(str);
+
+        //when
+        int year = date.getYear();
+        int month = date.getMonthValue();
+        int dayOfMonth = date.getDayOfMonth();
+
+        //then
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(year).isEqualTo(2022);
+        softAssertions.assertThat(month).isEqualTo(2);
+        softAssertions.assertThat(dayOfMonth).isEqualTo(9);
+        softAssertions.assertAll();
     }
 }
