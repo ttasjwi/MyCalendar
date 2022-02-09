c91b7575f731dcc0721dfb9d9d2ce21986db72b7
ttasjwi <ttasjwi920@gmail.com>
2022-02-09 18:15:27 +0900

    getFirstDayOfWeek, getMonthCalendar 메서드 추가

--- src/main/java/com/pair/MyCalelendar/MyCalendar.java
+++ src/main/java/com/pair/MyCalelendar/MyCalendar.java
@@ -1,16 +1,17 @@
 package com.pair.MyCalelendar;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
+import java.time.temporal.ChronoField;
 
 public class MyCalendar {
 
     public static LocalDateTime now() {
         LocalDateTime now = LocalDateTime.now();
         return LocalDateTime.of(now.getYear(), now.getMonthValue(), now.getDayOfMonth(),
-            now.getHour(), now.getMinute(), now.getSecond());
+                now.getHour(), now.getMinute(), now.getSecond());
     }
 
     public static LocalDate of(String str) {
@@ -25,5 +26,25 @@ public class MyCalendar {
     public static String formatToKorean(LocalDate localDate) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");
         return localDate.format(formatter);
+    }
+
+    public static DayOfWeek getFirstDayOfWeek(int year, int month) {
+        LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
+        return firstDayOfMonth.getDayOfWeek();
+    }
+
+    public static LocalDate[][] getMonthCalender(int year, int month) {
+        LocalDate[][] result = new LocalDate[5][7];
+        int monthLength = LocalDate.of(year, month, 1).lengthOfMonth();
+        int week = 1;
+        for (int j = 1; j <= monthLength; j++) {
+            LocalDate date = LocalDate.of(year, month, j);
+            int dayOfWeek = date.getDayOfWeek().getValue();
+            result[week - 1][dayOfWeek - 1] = date;
+            if (dayOfWeek==7) {
+                week++;
+            }
+        }
+        return result;
     }
 }
--- src/test/java/com/pair/MyCalelendar/MyCalendarTest.java
+++ src/test/java/com/pair/MyCalelendar/MyCalendarTest.java
@@ -1,13 +1,17 @@
 package com.pair.MyCalelendar;
 
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class MyCalendarTest {
@@ -69,5 +73,44 @@ class MyCalendarTest {
 
         //then
         assertThat(result).isEqualTo("2022년 07월 18일");
+    }
+
+    @Test
+    @DisplayName("해당 년월의 1일의 요일을 리턴한다.")
+    void getFirstDayOfWeek() {
+        //given
+        DayOfWeek firstDayOfWeek = MyCalendar.getFirstDayOfWeek(2022, 2);
+
+        //when
+        DayOfWeek expect = DayOfWeek.TUESDAY;
+
+        //then
+        assertThat(firstDayOfWeek).isEqualTo(expect);
+    }
+
+    @Test
+    @DisplayName("getMonthCalendar - 해당 월의 최대 일수만큼 LocalDate들이 존재해야한다.")
+    void getMonthCalender() {
+
+        //given
+        int year = 2022;
+        int month = 2;
+
+        int expectLengthOfMonth = MyCalendar.of(year, month, 1).lengthOfMonth();
+
+        LocalDate[][] monthCalender = MyCalendar.getMonthCalender(year, month);
+
+        int actualLengthOfMonth = 0;
+
+        for (int i=0; i<monthCalender.length; i++) {
+            for (int j=0; j<monthCalender[i].length; j++) {
+                if (monthCalender[i][j] != null) {
+                    actualLengthOfMonth ++;
+                }
+            }
+        }
+
+        //then
+        assertThat(actualLengthOfMonth).isEqualTo(expectLengthOfMonth);
     }
 }
